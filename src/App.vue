de129e1694d6550de6e7cc52087cdc331561673b
rodercode <rodersnow@gmail.com>
2023-05-23 19:16:08 +0200

    add weather app template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <template>
-  <h1>hello world</h1>
+  <div class="container-main">
+    <section class="section-history">
+      <div class="box-city">city_1</div>
+      <div class="box-city">city_2</div>
+      <div class="box-city">city_3</div>
+    </section>
+
+    <input class="search-field" placeholder="Search Field" type="text" />
+    <div class="container-weather-data">
+      <section class="section-temp">
+        <span class="temp-icon">Icon</span>
+        <span class="temp-data">Temparture</span>
+      </section>
+
+      <p class="paragraph-weather-info">weather description</p>
+
+      <section class="section-additonal-weather-info">
+        <span class="humidity">humidity</span>
+        <span class="pressure">pressure</span>
+        <span class="wind">wind</span>
+        <span class="speed">speed</span>
+      </section>
+    </div>
+
+    <h3 class="subtitle-hourly-forecast">Hourly forecast</h3>
+    <div class="container-hourly-forecast">
+      <ul class="list-hourly-forecast">
+        <li class="item-hourly-forecast">icon</li>
+        <li class="item-hourly-forecast">tempature</li>
+        <li class="item-hourly-forecast">time</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,12 +44,6 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
